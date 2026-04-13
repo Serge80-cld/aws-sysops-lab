24ba0ae6c9bfdc919893a603bd0248ddfd2fb62b
Serge80-cld <sergengindu1@yahoo.fr>
2026-04-13 19:13:08 +0200

    Create variables.tf

---
+++ terraform/terraform/modules/terraform/modules/security/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type        = string
+  description = "Nom du projet"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environnement (dev/staging/prod)"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "Région AWS"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID du VPC"
+}
